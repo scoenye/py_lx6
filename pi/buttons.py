e610175610744a5bb0c8a543446d27f47bedcb3f
Sven Coenye <scoenye@compaqnet.be>
2020-03-29 03:41:16 +0000

    Start pi package with simple button.

---
+++ pi/buttons.py
@@ -0,0 +1,45 @@
+"""
+    py_lx6
+    Raspberry Pi base hand controller for Meade LX-6 telescopes
+
+    Copyright (C) 2020  Sven Coenye
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or any
+    later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import RPi.GPIO as GPIO
+
+
+class Button:
+	"""
+	A single on/off button
+	"""
+
+	def __init__(self, pin):
+		self._pin = pin
+		GPIO.setup(pin, GPIO.out)
+
+	def on(self):
+		"""
+		Press the button
+		"""
+		GPIO.output(pin, 1)
+
+	def off(self):
+		"""
+		Release the button
+		"""
+		GPIO.output(pin, 0)
+
+	
