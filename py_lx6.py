796430ab85178835e907fdd12dbed25aa4ec5aa5
Sven Coenye <scoenye@compaqnet.be>
2020-03-29 10:18:39 -0400

    Fixup: use tabs for indentation, not spaces.

--- py_lx6.py
+++ py_lx6.py
@@ -1,21 +1,21 @@
 """
-    py_lx6
-    Raspberry Pi base hand controller for Meade LX-6 telescopes
+	py_lx6
+	Raspberry Pi base hand controller for Meade LX-6 telescopes
 
-    Copyright (C) 2020  Sven Coenye
+	Copyright (C) 2020  Sven Coenye
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or any
-    later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or any
+	later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import sys
@@ -24,15 +24,15 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class LX6UI(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(100, 100, 460, 320)        # sized to fit a PiTFT screen
-        self.setWindowTitle('PyNSD')
+	def __init__(self):
+		super().__init__()
+		self.setGeometry(100, 100, 460, 320)        # sized to fit a PiTFT screen
+		self.setWindowTitle('PyNSD')
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    form = LX6UI()
-    form.show()
-    # without this, the script exits immediately.
-    sys.exit(app.exec_())
+	app = QApplication(sys.argv)
+	form = LX6UI()
+	form.show()
+	# without this, the script exits immediately.
+	sys.exit(app.exec_())
